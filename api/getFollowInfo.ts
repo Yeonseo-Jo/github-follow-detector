93f8e8a8f0f22e566da7e1e34824f252ae624ab5
Yeonseo-Jo <jys073190@gmail.com>
2023-12-11 03:35:42 +0900

    fix : 팔로우 정보 가져오는 api 함수 타입 추가

--- api/getFollowInfo.ts
+++ api/getFollowInfo.ts
@@ -1,13 +1,14 @@
+import { UserTypes } from "@/types/User";
 import { client } from ".";
 
 const PER_PAGE = 100;
 
 export const getFollowInfo = async (token: string) => {
-  const { data: followingData } = await client(token).get(
+  const { data: followingData } = await client(token).get<Array<UserTypes>>(
     `/user/following?per_page=${PER_PAGE}`
   );
 
-  const { data: followersData } = await client(token).get(
+  const { data: followersData } = await client(token).get<Array<UserTypes>>(
     `user/followers?per_page=${PER_PAGE}`
   );
 
--- types/User.ts
+++ types/User.ts
@@ -1,7 +1,13 @@
 export interface UserProfileInfoTypes {
-  login: string;
-  bio: string;
-  avatar_url: string;
-  followers: number;
-  following: number;
+  login?: string;
+  bio?: string;
+  avatar_url?: string;
+  followers?: number;
+  following?: number;
+}
+
+export interface UserTypes {
+  login?: string;
+  bio?: string;
+  avatar_url?: string;
 }
