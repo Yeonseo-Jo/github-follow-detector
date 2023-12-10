a481987ec9c9cdeac9f5473f870570190b6f1d5d
Yeonseo-Jo <jys073190@gmail.com>
2023-12-11 03:10:04 +0900

    feat : 유저 프로필 가져오기

--- app/follow-list/page.tsx
+++ app/follow-list/page.tsx
@@ -1,7 +1,48 @@
+"use client";
+
+import { useGetUserProfile } from "@/hooks/user/useGetUserProfile";
+import Image from "next/image";
+import { useSearchParams } from "next/navigation";
 import React from "react";
 
 const page = () => {
-  return <div>팔로우 리스트 페이지</div>;
+  const searchParams = useSearchParams();
+  const userToken = searchParams.get("token");
+
+  if (!userToken) return;
+  const { isLoading, error, data } = useGetUserProfile(userToken);
+
+  console.log(isLoading, error, data);
+
+  if (!data) return;
+  const { login, avatar_url, bio, followers, following } = data;
+
+  return (
+    <div>
+      팔로우 리스트 페이지
+      {/* 유저 정보 부분! */}
+      <article>
+        {login}
+        <Image
+          src={avatar_url}
+          width={100}
+          height={100}
+          alt="유저-프로필-이미지"
+        />
+        {bio}
+        <div>
+          <p>
+            <span>팔로워 : </span>
+            <span>{followers}</span>
+          </p>
+          <p>
+            <span>팔로잉 : </span>
+            <span>{following}</span>
+          </p>
+        </div>
+      </article>
+    </div>
+  );
 };
 
 export default page;
