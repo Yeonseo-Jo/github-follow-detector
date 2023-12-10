d24a96b251717bb6f7ad53d11191e08ae2c4f5dd
Yeonseo-Jo <jys073190@gmail.com>
2023-12-11 00:16:49 +0900

    init : api axios instace 정의

---
+++ api/index.ts
@@ -0,0 +1,15 @@
+import axios, { AxiosInstance } from "axios";
+
+export const client = (token: string) => {
+  const client: AxiosInstance = axios.create({
+    baseURL: process.env.NEXT_PUBLIC_BASE_URL,
+    withCredentials: true,
+    headers: {
+      Accept: "application/vnd.github+json",
+      Authorization: `Bearer ${token}`,
+      "X-GitHub-Api-Version": "2022-11-28",
+    },
+  });
+
+  return client;
+};
