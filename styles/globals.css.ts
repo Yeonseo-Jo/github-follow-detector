176710a22f93e69b2d70bd1376c4ad02e88aa398
seoAreum <1971236@hansung.ac.kr>
2023-12-12 17:28:32 +0900

    style: 폰트 설정

--- styles/globals.css.ts
+++ styles/globals.css.ts
@@ -1,13 +1,20 @@
-import { globalStyle } from "@vanilla-extract/css";
+import { globalStyle, globalFontFace } from "@vanilla-extract/css";
 import { reset } from "./reset.css";
 
 export const GlobalStyle = [
   ...reset,
+  globalFontFace("insungitCutelivelyjisu", {
+    src: "url('https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_11-01@1.0/insungitCutelivelyjisu.woff2') format('woff2')",
+    fontWeight: "normal",
+    fontStyle: "normal",
+  }),
+
   globalStyle("*, *:before, *:after", {
     boxSizing: "border-box",
     WebkitTapHighlightColor: "rgba(255,255,255,0)",
     userSelect: "none",
     WebkitTouchCallout: "none",
+    fontFamily: "insungitCutelivelyjisu",
   }),
 
   globalStyle("input, textarea, button", {
